482f40149dbb697fb2ff6d93fae7d224e7c3570b
Alex Kup <116945542+Lifailon@users.noreply.github.com>
2023-10-25 14:55:23 +0300

    edit param type dir

--- Scripts/Install-Module.ps1
+++ Scripts/Install-Module.ps1
@@ -1,8 +1,8 @@
 $path = $(($env:PSModulePath -split ";")[0]) + "\Get-Sensor"
 if (Test-Path $path) {
     Remove-Item $path -Force -Recurse
-    New-Item -Path $path
+    New-Item -ItemType Directory -Path $path
 } else {
-    New-Item -Path $path
+    New-Item -ItemType Directory -Path $path
 }
 Invoke-RestMethod https://raw.githubusercontent.com/Lifailon/SensorsToInfluxDB/rsa/Get-Sensor/Get-Sensor.psm1 -OutFile "$path\Get-Sensor.psm1"
